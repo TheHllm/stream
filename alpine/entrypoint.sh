b92dcf960c0d6b4b0d67baa9525427ba7004e7e0
TheHllm <hllmollenkopf@gmail.com>
2020-06-10 13:35:11 +0000

    convert files in apline, moves uploadfolder

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 rm -rf /opt/dash/*
+
+mkdir /opt/uploads
 chmod 777 -R /opt/uploads
 
+mkdir /opt/convert
+chmod 777 -R /opt/convert
+
 #convert all .avi files
-cd /opt/uploads/
+cd /opt/convert/
 while true; do 
-    for f in *.avi; do 
+    for f in *; do 
         [ -f "$f" ] || continue
-        ffmpeg -i "$f"  "${f%.avi}.mp4"
+        ffmpeg -loglevel quiet -i "$f"  "/opt/uploads/${f%.*}.mp4"
+        rm "$f"
         echo $f
     done
 done
--- nginx/html/watch2gether/helper.inc.php
+++ nginx/html/watch2gether/helper.inc.php
@@ -1,38 +1,37 @@
 <?php
 class FileSaver{
+    const uploadPath = '/opt/uploads/';
+    const convertPath = "/opt/convert/";
     private function returnError($msg){
         echo json_encode(array( 'error' => $msg));
         exit;
     }
 
     
-    
     public function injestFile($inpath, $isUpload, $redirect){
 
-        //calculate hash
+        //calculate hash to be used as an uid
         $hash = hash_file('md5', $inpath);
+
+        $path = self::convertPath; // move file to convert by default
 
         //get name and path
         $filetype = exec('file '. $inpath);
         if(strpos($filetype, 'MP4') !== false){
-            $extension = "mp4";
+            $extension = 'mp4';
+            $path = self::uploadPath; //dont move to convert
         }else if(strpos($filetype, 'Macromedia Flash Video') !== false){
             $extension = 'flv';
         }else if(strpos($filetype, 'AVI') !== false){
             $extension = 'avi';
+        }else if(strpos($filetype, 'Matroska') !== false){
+            $extension = 'mkv';
         }else{
-            $this->returnError("Has to be mp4,flv or avi!");
+            $this->returnError("Has to be mp4, flv, mkv, or avi. But it was " . $filetype);
         }
 
-        $filename = $hash . '.' . $extension;
-        $path = '/opt/uploads/' ;
-        $filePath = $path . $filename;
-
-
-        //make dir
-        if (!is_dir($path)) {
-            mkdir($path);
-        }
+        
+        $filePath = $path . $hash . '.' . $extension;
 
         if(!file_exists($filePath)){
             if($isUpload){
@@ -41,10 +40,12 @@ class FileSaver{
                 rename($inpath, $filePath);
             }
         }
+
+        $fileId = $hash . '.mp4';
         if($redirect){
-            header("Location: watch?v=" . $filename);
+            header("Location: watch?v=" . $fileId);
         }
-        return $hash;
+        return $fileId;
     }
 }
 ?>
